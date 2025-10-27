a4faa4de58b8591286f58292dc1a5e8bd480b4d4
SensibleN00B <sensible.noob@gmail.com>
2025-10-27 17:34:55 +0200

    chore: Update column lengths for `case_number` and `case_proc` to 255 in `cases` table

--- src/database/models.py
+++ src/database/models.py
@@ -11,8 +11,8 @@ class Case(Base):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     court_name: Mapped[str] = mapped_column(String(255))
-    case_number: Mapped[str] = mapped_column(String(100), unique=True, index=True)
-    case_proc: Mapped[str] = mapped_column(String(100), nullable=True)
+    case_number: Mapped[str] = mapped_column(String(255), unique=True, index=True)
+    case_proc: Mapped[str] = mapped_column(String(255), nullable=True)
     registration_date: Mapped[Date] = mapped_column(Date, nullable=True)
     judge: Mapped[str] = mapped_column(String(255), nullable=True)
     judges: Mapped[str] = mapped_column(Text, nullable=True)
